8028cfd4125d8be3882aa87ff0d5bec17a4480cd
chenyangkang <chenyangkang@outlook.com>
2023-03-13 20:54:53 +0800

    change

--- weibo_base/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-
-"""
- Author: Helixcs
- Site: https://iliangqunru.bitcron.com/
- File: __init__.py.py
- Time: 5/19/18
-"""
-from .weibo_api import *
--- weibo_base/weibo_util.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-
-"""
- Author: Helixcs
- Site: https://iliangqunru.bitcron.com/
- File: weibo_util.py
- Time: 5/19/18
- Descripton:  weibo_util is in common use
-"""
